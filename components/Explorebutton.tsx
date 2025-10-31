af7f35b1a52ac7bd8cd828c0e43a8ad92ca9b7d7
Hp <rs739025@gmail.com>
2025-11-01 03:40:31 +0530

    inital posthog

--- components/Explorebutton.tsx
+++ components/Explorebutton.tsx
@@ -1,20 +1,16 @@
 'use client';
 
-import React from 'react'
-import Image from 'next/image';
+import Image from "next/image";
 
-const Explorebutton = () => {
-  return (
-    <div>
-      <button type='button' id='explore-btn' className='mt-7 mx-auto' onClick={() => console.log('Click')}>
-        <a href="#events">
-            Explore Events
-            <Image src="/icons/arrow-down.svg" alt="arrow-down" width={20} height={20} />
-        </a>
-        
-    </button>
-    </div>
-  )
+const ExploreBtn = () => {
+    return (
+        <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={() => console.log('CLICK')}>
+            <a href="#events">
+                Explore Events
+                <Image src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24} />
+            </a>
+        </button>
+    )
 }
 
-export default Explorebutton
+export default ExploreBtn
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
   experimental: {
     turbopackFileSystemCacheForDev: true,
   },
+
+  // Add PostHog rewrites
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
+  },
+
+  // Support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
