61d08a156da525a36e0d6086f13b9007bfa6a33d
Hp <rs739025@gmail.com>
2025-11-16 03:17:55 +0530

    routes and frontend fuctionality

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,17 +1,93 @@
 import { notFound } from "next/navigation"
+import Image from "next/image";
+import BookEvent from "@/components/BookEvent";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-const EventDetailPage = async({params}: {params: Promise<{slug: string}>}) => {
+const EventDetailItem = ({icon,alt,lable}: {icon:string, alt:string, lable:string}) => (
+  <div className="flex-row-gap-2">
+    <Image src={icon} alt={alt} width={14} height={14} />
+    <p>{lable}</p>
+  </div>
+)
+const EventAgenda = ({agendaItem} : {agendaItem: string[]}) => (
+  <div className="agenda">
+    <h2>Agenda</h2>
+      <ul>
+        {agendaItem.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+  </div>
+) 
+const EventTags = ({tags}: {tags: string[]}) => (
+  <div className="flex flex-row gap-1.5 flex-wrap">
+    {tags.map((tag) => (
+      <div className="pill" key={tag}>{tag}</div>
+    ))}
+  </div>
+)
+
+const bookings = 10;
+
+const EventDetailPage = async( {params}: {params: Promise<{slug: string}>}) => {
   const {slug} = await params;
   const request = await fetch(`${BASE_URL}/api/events/${slug}`);
-  const {data} = await request.json();
+  const { event: { description, date, time, location, image, mode, agenda, audience, overview, tags, organizer }} = await request.json();
 
-  if(!data) return notFound();
+  if(!description) return notFound();
   return (
     <section id="event">
-        <h1>Event Details: <br />{slug}</h1>
+      <div className="header">
+        <h1>Event Description</h1>
+        <p>{description}</p>
+      </div>
+
+        <div className="details">
+          {/* Event Image */}
+          <div className="content">
+            <Image src={image} alt='Event Banner' width={800} height={800} className="banner" />
+
+            <section className="flex-col-gap-2">
+              <h2>Overview</h2>
+              <p className="mt-2">{overview}</p>
+            </section>
+            <section className="flex-col-gap-2">
+              <h2>Event Details</h2>
+              <EventDetailItem icon="/icons/calendar.svg" alt="date" lable={date} />
+              <EventDetailItem icon="/icons/clock.svg" alt="time" lable={time} />
+              <EventDetailItem icon="/icons/pin.svg" alt="location" lable={location} />
+              <EventDetailItem icon="/icons/mode.svg" alt="mode" lable={mode} />
+              <EventDetailItem icon="/icons/audience.svg" alt="audience" lable={audience} />
+            </section>
+
+            <EventAgenda agendaItem={JSON.parse(agenda)} />
+
+            <section className="flex-col-gap-2">
+              <h2>About The Organizer</h2>
+              <p>{organizer}</p>
+            </section>
+            <EventTags tags={JSON.parse(tags)} />
+            
+          </div>
+
+          {/* Event Overview */}
+          <aside className="flex-1 w-full p-4 border-l border-gray-700">
+            <div className="bg-dark-100 border-dark-200 card-shadow flex w-full flex-col gap-6 rounded-[10px] border px-5 py-6">
+              <h2>Book Your Seat</h2>
+              {bookings > 0 ? (
+                <p className="text-sm">
+                  Join {bookings} people who have already booked their spot!
+                  </p>
+              ) : (
+                <p className="text-sm">Be The First to Book Your Spot!</p>
+              )}
+              <BookEvent />
+            </div>      
+          </aside>
+        </div>
     </section>
+    
   )
 }
 
---
+++ components/BookEvent.tsx
@@ -0,0 +1,40 @@
+'use client'
+import { useState } from 'react';
+
+
+const BookEvent = () => {
+   const [email, setEmail] = useState('');
+   const [submitted, setSubmitted] = useState(false);
+
+   const handleSubmit = (e: React.FormEvent) => {
+       e.preventDefault();
+       setTimeout(() => {
+           setSubmitted(true);
+       },1000)
+   }
+
+  return (
+    <div>
+        {submitted ? (
+            <p className='text-sm'>Thank you for booking!</p>
+        ) : (
+            <form onSubmit= {handleSubmit}>
+                <div>
+                    <label htmlFor="email" className="email">Email address</label>
+                    <input 
+                    
+                        type="email" 
+                        id="email" 
+                        value={email}
+                        placeholder="Enter your email"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <button type="submit" className="button-submit">Submit</button>
+            </form>
+        )}
+    </div>
+  )
+}
+
+export default BookEvent
